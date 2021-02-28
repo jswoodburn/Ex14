6934afc867fd1f38f736d689c2834b7fd0330471
jackie <jackie.woodburn@gmail.com>
2021-02-28 14:46:03 +0000

    added alternate while loop to game file

--- rps_game.py
+++ rps_game.py
@@ -1,8 +1,17 @@
 import rps_functions
 
-play_again = 'Y'
-while play_again == 'Y': 
+# play_again = 'Y'
+# while play_again == 'Y':
+#     user_choice = rps_functions.get_user_choice()
+#     user_choice_converted=rps_functions.convert_user_choice(user_choice)
+#     print(user_choice_converted)
+#     play_again = rps_functions.get_user_choice("Do you want to play again (Y/N): ", ['Y','N'])
+
+
+while True:
     user_choice = rps_functions.get_user_choice()
     user_choice_converted=rps_functions.convert_user_choice(user_choice)
     print(user_choice_converted)
     play_again = rps_functions.get_user_choice("Do you want to play again (Y/N): ", ['Y','N'])
+    if play_again == 'N':
+        break
