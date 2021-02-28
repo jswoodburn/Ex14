46bc157d9ef9faae409f544c2fa346455c9a8fcf
Jessie Auguste <jessieauguste@gmail.com>
2021-02-28 14:56:08 +0000

    added the computer choice

--- rps_functions.py
+++ rps_functions.py
@@ -1,3 +1,4 @@
+import random
 # get user input
 def get_user_choice(question_string = "Enter your choice (r, p, or s): ", acceptable_answer = ['R','P','S']):
     while True:  # fails after 3 attempts?
@@ -19,9 +20,15 @@ def convert_user_choice(user_choice):
 
 
 # generate random number
-
-
+def generate_comp_choice():
+    comp_choice = random.randint(0,2)
 # convert number to computer choice
+    if comp_choice == 0:
+        return 'rock'
+    elif comp_choice == 1:
+        return 'scissors'
+    else:
+        return 'paper'
 
 
 # compare choices/decide winner
