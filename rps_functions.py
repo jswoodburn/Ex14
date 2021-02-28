e2c3e13efd75c261827de58d9f07ba77cd5c3edd
jackie <jackie.woodburn@gmail.com>
2021-02-28 15:20:42 +0000

    added function to determine winner. edited game file to give extra features beyond MVP

--- rps_functions.py
+++ rps_functions.py
@@ -1,27 +1,27 @@
 import random
+
+
 # get user input
-def get_user_choice(question_string = "Enter your choice (r, p, or s): ", acceptable_answer = ['R','P','S']):
+def get_user_choice(question_string="\nEnter your choice (r, p, or s): ", acceptable_answer=['R', 'P', 'S']):
     while True:  # fails after 3 attempts?
         user_choice = input(question_string)
         if user_choice.upper() in acceptable_answer:
             return user_choice.upper()
         print(f"Your choice must be one of the letters: {acceptable_answer}.\n")
 
+
 # convert user choice to word
 def convert_user_choice(user_choice):
-    full_words={'R':'rock',
-                'P':'paper',
-                'S':'scissors'
+    full_words= {'R': 'rock',
+                'P': 'paper',
+                'S': 'scissors'
                 }
     return full_words[user_choice]
 
 
-
-
-
 # generate random number
 def generate_comp_choice():
-    comp_choice = random.randint(0,2)
+    comp_choice = random.randint(0, 2)
 # convert number to computer choice
     if comp_choice == 0:
         return 'rock'
@@ -31,8 +31,19 @@ def generate_comp_choice():
         return 'paper'
 
 
+win_condition = {
+    'rock': 'scissors',
+    'scissors': 'paper',
+    'paper': 'rock'
+}
+
+
 # compare choices/decide winner
-
-
-# print winner
+def get_winner(user_choice, comp_choice):
+    if user_choice == comp_choice:
+        return [0, "It's a draw!"]
+    elif win_condition[user_choice] == comp_choice:
+        return [1, "You win!"]
+    else:
+        return [0, "You lose..."]
 
--- rps_game.py
+++ rps_game.py
@@ -1,17 +1,32 @@
 import rps_functions
+import time
 
-# play_again = 'Y'
-# while play_again == 'Y':
-#     user_choice = rps_functions.get_user_choice()
-#     user_choice_converted=rps_functions.convert_user_choice(user_choice)
-#     print(user_choice_converted)
-#     play_again = rps_functions.get_user_choice("Do you want to play again (Y/N): ", ['Y','N'])
-
-
+games_played = 0
+games_won = 0
 while True:
+    # get user input as a letter and convert to word
     user_choice = rps_functions.get_user_choice()
     user_choice_converted=rps_functions.convert_user_choice(user_choice)
-    print(user_choice_converted)
+
+    # generate comp choice
+    comp_choice = rps_functions.generate_comp_choice()
+
+    # choose winner
+    print(f'You chose {user_choice_converted}...')
+
+    print(f'The computer chooses...')
+    time.sleep(2)
+    print(f'{comp_choice.capitalize()}!\n')
+    win_list = rps_functions.get_winner(user_choice_converted, comp_choice)
+    print(win_list[1])
+
+    # track how many games played
+    games_won+=win_list[0]
+    games_played+=1
+
+    # does user want to play again?
     play_again = rps_functions.get_user_choice("Do you want to play again (Y/N): ", ['Y','N'])
     if play_again == 'N':
         break
+
+print(f"\nYou won {games_won} out of {games_played} games that you played.")
